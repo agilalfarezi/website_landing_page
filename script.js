095ee34ec3f47d0a897bc36029b9862fe6231b57
Agil Alfarezi <agilalfarezi02@gmail.com>
2025-12-23 12:58:38 +0700

    update tampilan / fitur

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const discordLink = "https://discord.gg/arezstoreofficial";
+const discordLink = "https://discord.gg/INVITE_KAMU";
 
 document.getElementById("joinDiscord").addEventListener("click", () => {
   window.open(discordLink, "_blank");
